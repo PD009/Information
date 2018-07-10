ba54d81afc59fab03627bfab2944bfe3211679f9
Priyadarshini Basker <priyadarshinib977@gmail.com>
2018-07-10 11:25:58 +0530

    syntaxx

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,9 @@
 ﻿'use strict';
 var express = require('express');
 var router = express.Router();
-const bodyParser = require("body-parser");
 
-//var pg = require("pg");
-const restService = express();
-
-restService.use(
-    bodyParser.urlencoded({
-      extended: true
-    })
-  );
-  restService.use(bodyParser.json());
 /* GET home page. */
-restService.post('/demo', function (req, res) {
+router.post('/demo', function (req, res) {
     /*var userName = req.queryResult.username;
     var phoneNo = req.queryResult.phone_no;
     var email = req.queryResult.email_add;
@@ -27,11 +17,8 @@ restService.post('/demo', function (req, res) {
     var aadhar = req.body.aadhar;
     console.log("request params", req)*/
     return res.json({
-        fulfillmentText:"Nice to know your details"
+        fulfillmentText:"Nice to know your details";
      });
-    });
-    restService.listen(process.env.PORT || 3000, function() {
-        console.log("Server up and listening");
     });
     
 
